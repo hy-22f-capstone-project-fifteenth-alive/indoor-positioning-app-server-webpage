ee15f224c49dc99f05d3579b9124d7035a9ee4c1
Sooyong Shin <shinsy1023@gmail.com>
2023-02-23 11:06:40 +0900

    [추가] Google OAuth2 로그인 버튼 테스트

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const clientId = "532205455452-8v9g5lghea3po322jujds7dkv4sl0tbe.apps.googleusercontent.com";
+
+	function init() {
+	}
+
+	var url = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&`
+	url += `client_id=${clientId}`;
+	url += `&scope=openid%20email`;
+	url += `&redirect_uri=http://localhost:8080/login/oauth2/code/google`;
+	url += `&state=security_token%3D138r5719ru3e1%26url%3Dhttp://localhost:3000`;
+	url += `&nonce=testNonce`
+
+	const onSuccess = function (res) {
+		console.log(res);
+
+	}
+
+	const onFailure = function (res) {
+		console.log(res);
+	}
+	
+	return (
+		<div className="App">
+			<button
+			type="button"
+			onClick={(e) => {
+				e.preventDefault();
+				window.location.href=url;
+			}}
+			>구글 로그인</button>
+		</div>
+		);
 }
 
 export default App;
